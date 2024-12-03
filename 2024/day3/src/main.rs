e6674b2e3b28356fed24cf1a65b6fbdd815a9792
Matteo Fava <matteo.luca.fava@gmail.com>
2024-12-03 23:41:05 +0100

    day3 2024

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     println!("Part one: {ans1}");
     assert!(part_one("data/input.txt") == 169021493);
 
+    assert!(part_two("data/sample.txt") == 48);
     let ans2 = part_two("data/input.txt");
     println!("Part two: {ans2}");
 }
@@ -26,6 +27,20 @@ fn part_one(file: &str) -> i32 {
 }
 
 fn part_two(file: &str) -> i32 {
-    todo!()
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)").unwrap();
+    let mut do_ = true;
+    let mut acc: i32 = 0;
+    read_to_string(file).unwrap().lines().into_iter().for_each(|l| {
+        re.captures_iter(l).for_each(|c| {
+            match &c[0] {
+                "do()" => do_ = true,
+                "don't()" =>  do_ = false,
+                _ =>  if do_ {
+                    let n: Vec<i32> = c[0].replace("mul(", "").replace(")", "").trim().split(",").map(|n| n.parse::<i32>().unwrap()).collect();
+                    acc += n[0] * n[1];
+                },
+            }
+        });
+    });
+    acc
 }
-
